386e4496f49d92b3e7fbe68b8070f734868eb709
AHMED <eng.computer1@yahoo.com>
2021-03-07 23:42:21 -0600

    added modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import Appointments from './components/appointments'  
 import CreateAppointment from './components/createappointment'  
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import React, { useState } from 'react';
 
 function App() {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="App">
+   <div className="App">
       <Appointments/>
-      <CreateAppointment/>
+     
+    
+       <Modal
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={<Button>Add Appointment</Button>}
+        >
+        
+      <Modal.Header>Add Appointment</Modal.Header>
+          <Modal.Content >
+            <CreateAppointment/>
+          
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color='black' onClick={() => setOpen(false)}>
+              Close
+            </Button>
+          </Modal.Actions>
+    </Modal>
     </div>
+
   );
 }
 
